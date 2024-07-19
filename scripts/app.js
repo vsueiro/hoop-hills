e835a4fc53f57cbf35d95bf74ebfa331ad8c8a1a
Vinicius Sueiro <hi@vsueiro.com>
2024-07-19 19:44:31 -0400

    Begin organizing JS into classes

--- scripts/app.js
+++ scripts/app.js
@@ -1,68 +1,14 @@
 import { csv } from "d3";
 
-window.app = {};
+import Filters from "./classes/Filters.js";
 
-class Filters {
-  constructor(app) {
-    this.app = app;
-    this.form = document.querySelector("form");
-
-    this.team = "";
-    this.opponent = "";
-    this.season = "";
-    this.sorting = "";
-    this.games = [];
-    this.results = [];
-    this.periods = [];
-
-    this.allValues = {
-      opponent: "all",
-      games: ["RS", "PI", "PO"],
-      results: ["won", "lost"],
-      periods: ["Q1", "Q2", "Q3", "Q4", "OTs"],
-    };
-
-    this.form.addEventListener("input", () => {
-      this.updateFiltersValues();
-    });
-
-    this.form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      this.updateFiltersValues();
-    });
-
-    this.updateFiltersValues();
-  }
-
-  isAll(field) {
-    if (typeof this[field] === "string") {
-      return this[field] === this.allValues[field];
-    }
-
-    const isArrayEqual =
-      this[field].length === this.allValues[field].length &&
-      this[field].every((value, index) => value === this.allValues[field][index]);
-
-    return isArrayEqual;
-  }
-
-  updateFiltersValues() {
-    const formData = new FormData(this.form);
-
-    this.team = formData.get("team");
-    this.opponent = formData.get("opponent");
-    this.season = formData.get("opponent");
-    this.sorting = formData.get("sorting");
-
-    this.games = formData.getAll("games");
-    this.results = formData.getAll("results");
-    this.periods = formData.getAll("periods");
-
-    // console.log(this.isAll("periods"));
+class App {
+  constructor() {
+    this.filters = new Filters(this);
   }
 }
 
-app.filters = new Filters(app);
+window.app = new App();
 
 let data = [];
 let view = "corner";
