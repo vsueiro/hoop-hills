3262aad721ceb0e73bf52e2113bde39113f141e0
Vinicius Sueiro <hi@vsueiro.com>
2024-07-24 23:05:23 -0400

    Make corner view a bit flatter so label is further from viewport bottom edge

--- scripts/classes/World/Camera.js
+++ scripts/classes/World/Camera.js
@@ -15,7 +15,7 @@ export default class Camera {
       bars: { theta: Math.PI * 0.5, phi: Math.PI * 0.5 },
       grid: { theta: 0, phi: 0 },
       lines: { theta: 0, phi: Math.PI * 0.5 },
-      corner: { theta: Math.PI * 0.25, phi: Math.PI * 0.333 },
+      corner: { theta: Math.PI * 0.25, phi: Math.PI * 0.36 },
     };
 
     this.setup();
