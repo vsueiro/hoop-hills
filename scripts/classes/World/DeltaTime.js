a4241361e3c69eafe88114f7e46fc7b6050c0ed9
Vinicius Sueiro <hi@vsueiro.com>
2025-04-22 23:29:57 -0400

    Add fps on screen

--- scripts/classes/World/DeltaTime.js
+++ scripts/classes/World/DeltaTime.js
@@ -1,6 +1,7 @@
 export default class DeltaTime {
   constructor(lastTime) {
     this.lastTime = lastTime === undefined ? 0 : lastTime;
+    this.fps = document.querySelector(".fps");
     this.value = 0;
   }
 
@@ -11,5 +12,7 @@ export default class DeltaTime {
   update(currentTime) {
     this.value = (currentTime - this.lastTime) / 1000;
     this.lastTime = currentTime;
+
+    if (this.fps) this.fps.textContent = "◼️".repeat(Math.round(0.1 / this.value));
   }
 }
